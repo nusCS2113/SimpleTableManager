17127452f7ea35ce5ad5f7ed76b609fd921a4bf9
Akshay Narayan <anarayan.soc@gmail.com>
2025-12-30 11:17:55 +0800

    Create TableManager

---
+++ src/main/java/TableManager.java
@@ -0,0 +1,9 @@
+public class TableManager {
+  
+    public static void main(String[] args) {
+        System.out.println("===============================================================================");
+        System.out.println("===============  Welcome!!! Simple Table Manager - Version 0.0  ===============");  
+        System.out.println("===============================================================================");
+    }
+
+}
