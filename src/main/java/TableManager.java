1cad4884814c50f655fce6606c2a59eef5df5572
Akshay Narayan @ Zorinbox <akshay@zorinbox.org>
2025-12-30 16:30:53 +0800

    Extract welcome message to a new method

--- src/main/java/TableManager.java
+++ src/main/java/TableManager.java
@@ -1,8 +1,12 @@
 public class TableManager {
   
     public static void main(String[] args) {
+        displayWelcome();
+    }
+
+    public static void displayWelcome() {
         System.out.println("===============================================================================");
-        System.out.println("===============  Welcome!!! Simple Table Manager - Version 0.0  ===============");  
+        System.out.println("===============  Welcome!!! Simple Table Manager - Version 0.0  ===============");
         System.out.println("===============================================================================");
     }
 
